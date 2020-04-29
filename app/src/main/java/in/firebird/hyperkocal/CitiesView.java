7d6a5d0f98bf4c2c23840965727c479b7cb43a92
neelratanguria <neelratan@live.com>
2020-04-29 18:09:54 +0530

    City page added

---
+++ app/src/main/java/in/firebird/hyperkocal/CitiesView.java
@@ -0,0 +1,63 @@
+package in.firebird.hyperkocal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static in.firebird.hyperkocal.Splash.list;
+
+public class CitiesView extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    private CitiesAdapter adapter;
+    private List<City> cityList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cities_view);
+
+        recyclerView = (RecyclerView) findViewById(R.id.myRecyclerViewCities);
+
+        cityList = new ArrayList<>();
+        adapter = new CitiesAdapter(this, cityList);
+
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 1);
+        recyclerView.setLayoutManager(mLayoutManager);
+        //recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(10), true));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+
+        prepareCities();
+    }
+
+    private void prepareCities() {
+
+        City a;
+
+        HashMap cityMap = (HashMap) list.get(0);
+
+        ArrayList cityArray = (ArrayList) cityMap.get("cities");
+
+        int numberOfCities = cityArray.size();
+        for (int i = 0; i<numberOfCities; i++)
+        {
+            HashMap cityDetails = (HashMap) cityArray.get(i);
+            a = new City((String) cityDetails.get("name"), (String) cityDetails.get("thumbnail"));
+            cityList.add(a);
+        }
+
+        adapter.notifyDataSetChanged();
+    }
+}
