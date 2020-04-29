92a35f08d8ae3a08f5c6b5024a92cd76e761e7ed
neelratanguria <neelratan@live.com>
2020-04-29 16:35:17 +0530

    State page done

--- app/src/main/java/in/firebird/hyperkocal/MainActivity.java
+++ app/src/main/java/in/firebird/hyperkocal/MainActivity.java
@@ -1,6 +1,9 @@
 package in.firebird.hyperkocal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -11,14 +14,33 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    private StatesAdapter adapter;
+    private List<StatesHandler> statesHandlerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.statecardview);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.myRecyclerView);
+
+        statesHandlerList = new ArrayList<>();
+        adapter = new StatesAdapter(this, statesHandlerList);
+
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 1);
+        recyclerView.setLayoutManager(mLayoutManager);
+        //recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(10), true));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+
+        prepareStates();
 
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -42,5 +64,11 @@ public class MainActivity extends AppCompatActivity {
                 Log.w("Neel", "Failed to read value.", error.toException());
             }
         });
+    }
+
+    private void prepareStates() {
+        StatesHandler a = new StatesHandler("Jharkhand", "https://img.etimg.com/thumb/msid-73464087,width-300,imgsize-288631,resizemode-4/jagannath-temple-ani.jpg");
+        statesHandlerList.add(a);
+        adapter.notifyDataSetChanged();
     }
 }
